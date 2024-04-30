43b699ea8c1bba00e603079dd6e938fb2e5c1b3d
sophie8909 <34826797+sophie8909@users.noreply.github.com>
2024-04-30 13:05:28 +0800

    read ntnu pdf to cvs

---
+++ reader.py
@@ -0,0 +1,15 @@
+
+import csv
+import pdfplumber
+pdf = pdfplumber.open('timetable.pdf')   # 開啟 pdf
+print(pdf.pages)                          
+page = pdf.pages[0]           # 讀取第一頁
+table = page.extract_table()    # 取出文字
+print(table)
+
+csvfile = open('test-csv.csv', 'w+', encoding="utf-8")  # 建立 CSV 檔案
+write = csv.writer(csvfile)           # 建立寫入物件
+for i in table:
+    write.writerow(i)                 # 讀取表格每一列寫入 CSV
+print('ok')
+pdf.close()
