ba1880b7e45f6e825a1092bb00e530082ba21823
蔡鳳駿 <eoleedimin@gmail.com>
2024-05-15 11:59:26 +0800

    refactor: improve parser performance & clearity

--- app/parser/ntnu.py
+++ app/parser/ntnu.py
@@ -18,33 +18,56 @@ zh_day_to_int = {
 
 class SimpleTimeTableParser:
     def dataframe_to_courses(self, data: pd.DataFrame) -> List[Course]:
-        courses = []
-        for i in range(len(data)):
-            course = Course(
-                name=data.iloc[i]["課程中文名稱"].replace("\n", ""),
-                start_time=datetime.datetime.strptime(
-                    data.iloc[i]["上課時間"].split("-")[0].strip(),
-                    "%H:%M",
-                ).time(),
-                end_time=datetime.datetime.strptime(
-                    data.iloc[i]["上課時間"].split("-")[1].strip(),
-                    "%H:%M",
-                ).time(),
-                location=data.iloc[i]["上課教室"].replace("\n", ""),
-                weekday=zh_day_to_int.get(data.iloc[i]["星期"], 0),
+        return self.combine_continuous_sessions(
+            [
+                Course(
+                    name=row["課程中文名稱"].replace("\n", ""),
+                    start_time=datetime.datetime.strptime(
+                        row["上課時間"].split("-")[0].strip(),
+                        "%H:%M",
+                    ).time(),
+                    end_time=datetime.datetime.strptime(
+                        row["上課時間"].split("-")[1].strip(),
+                        "%H:%M",
+                    ).time(),
+                    location=row["上課教室"].replace("\n", ""),
+                    weekday=zh_day_to_int.get(row["星期"], 0),
+                )
+                for _index, row in data.iterrows()
+            ]
+        )
+
+    def combine_continuous_sessions(
+        self, courses: List[Course], max_minutes_diff: int = 20
+    ) -> List[Course]:
+        def same_day_course(course1, course2):
+            return (
+                course1.name == course2.name
+                and course1.weekday == course2.weekday
+                and course1.location == course2.location
             )
-            courses.append(course)
-        
+
         for course in courses:
             for b_course in courses:
-                if course != b_course and course.name == b_course.name and course.weekday == b_course.weekday and course.location == b_course.location:
-                    
-                    datetime1 = datetime.datetime.combine(datetime.datetime.today(), course.end_time)
-                    datetime2 = datetime.datetime.combine(datetime.datetime.today(), b_course.start_time)
-                    if (datetime2 - datetime1).total_seconds() <= 20*60:
-                        course.start_time = min(course.start_time, b_course.start_time)
-                        course.end_time = max(course.end_time, b_course.end_time)
-                        courses.remove(b_course)
+                # Skip Identical courses
+                if course == b_course:
+                    continue
+
+                # Skip non-continuous sessions
+                if not same_day_course(course, b_course):
+                    continue
+
+                # Check if the courses are continuous
+                minutes_between = (
+                    (course.start_time.hour - b_course.end_time.hour) * 60
+                    + course.start_time.minute
+                    - b_course.end_time.minute
+                )
+                if minutes_between <= max_minutes_diff:
+                    # Combine continuous sessions
+                    course.start_time = min(course.start_time, b_course.start_time)
+                    course.end_time = max(course.end_time, b_course.end_time)
+                    courses.remove(b_course)
         return courses
 
     def table_to_dataframe(self, table: List[List[str | None]] | None) -> pd.DataFrame:
