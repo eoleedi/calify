2a6d20d464edf47c9a2c265d56755341c9e70469
蔡鳳駿 <eoleedimin@gmail.com>
2024-05-18 01:20:21 +0800

    refactor: move combine_continuous_sessions to utils.py

--- app/parser/ntnu.py
+++ app/parser/ntnu.py
@@ -4,6 +4,7 @@ from typing import List
 import datetime
 import pandas as pd
 from app.course import Course
+from app.utils import combine_continuous_sessions
 
 zh_day_to_int = {
     "一": 1,
@@ -18,7 +19,7 @@ zh_day_to_int = {
 
 class SimpleTimeTableParser:
     def dataframe_to_courses(self, data: pd.DataFrame) -> List[Course]:
-        return self.combine_continuous_sessions(
+        return combine_continuous_sessions(
             [
                 Course(
                     name=row["課程中文名稱"].replace("\n", ""),
@@ -36,39 +37,6 @@ class SimpleTimeTableParser:
                 for _index, row in data.iterrows()
             ]
         )
-
-    def combine_continuous_sessions(
-        self, courses: List[Course], max_minutes_diff: int = 20
-    ) -> List[Course]:
-        def same_day_course(course1, course2):
-            return (
-                course1.name == course2.name
-                and course1.weekday == course2.weekday
-                and course1.location == course2.location
-            )
-
-        for course in courses:
-            for b_course in courses:
-                # Skip Identical courses
-                if course == b_course:
-                    continue
-
-                # Skip non-continuous sessions
-                if not same_day_course(course, b_course):
-                    continue
-
-                # Check if the courses are continuous
-                minutes_between = (
-                    (course.start_time.hour - b_course.end_time.hour) * 60
-                    + course.start_time.minute
-                    - b_course.end_time.minute
-                )
-                if minutes_between <= max_minutes_diff:
-                    # Combine continuous sessions
-                    course.start_time = min(course.start_time, b_course.start_time)
-                    course.end_time = max(course.end_time, b_course.end_time)
-                    courses.remove(b_course)
-        return courses
 
     def table_to_dataframe(self, table: List[List[str | None]] | None) -> pd.DataFrame:
         return pd.DataFrame(table[1:], columns=table[0])
